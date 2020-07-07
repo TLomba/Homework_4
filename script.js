e0dee84e6fcc650412a0506983194490657fd952
T Lom <tiffanyplombardi@gmail.com>
2020-07-07 19:51:17 -0400

    HW4

---
+++ script.js
@@ -0,0 +1,103 @@
+// Dependencies DOM Elements ==============================
+var timerEl = document.querySelector(".timer");
+var startEl = document.querySelector("#start");
+
+// Time variable
+var secondsLeft = 10;
+
+  // var questions
+  // Var responses
+var startEl = document.querySelector("")
+
+
+// User Interaction =======================================
+// First Window with a Greeting called Coding Quiz Challenge that is timed
+
+// Start Button - create a button
+// Timer Starts
+startEl.addEventListener ("click", function() {
+    secondsLeft --;
+})
+startEL.addEventListener
+// because time is important we will need to use timernterval
+function setTime() {
+    var timeInterval = setInterval(function () {
+        console.log("it's working");
+        secondLeft--;
+        if (secondsLeft)
+        timeINterval.textContent = secondsLeft + " seconds left till colorspplosion"
+    })
+
+}
+
+var question = {
+    "a" : 
+}
+// Loop through the aray of questions each of which is an object - If Else
+    // Each question has multiple choice answers
+
+
+    // Title - Famous Scientists
+
+// Questions - and create radio button for the answers or have the button hold the answer
+
+var questions = [
+    "Who was Einstein?";
+    "Who was Max PLanck?";
+    "Who was Marie Curie?";
+    "Who was Nikola Tesla?";
+];
+
+var answer = [
+    {
+        q1 : "Who was Einstein?";
+        a: "The author of the Theory of Relativity and Special Relativity";
+        b: "He discovered the Law of Gravity";
+        c: "c - A Heavy Metal Guitarist"
+    },
+    {
+        q2: "Who was Max Planck?";
+        a: "Answer option";
+        b: "Answer option";
+        c: "Answer option";
+    }
+    {
+        q2: "Who was Marie Curie?";
+        a: "Answer option";
+        b: "Answer option";
+        c: "Answer option";
+    }
+    {
+        q2: "Who was Nikola Tesle?";
+        a: "Answer option";
+        b: "Answer option";
+        c: "Answer option";
+    }
+]
+
+    // Question 1 - Who was Einstein?
+        // a - The author of the Theory of Relativity and Special Relativity
+        // b - He discovered the Law of Gravity
+        // c - A Heavy Metal Guitarist
+    
+    // Question 2 - Who was Max PLanck?
+    //     a - Answer option
+    //     b - Answer option
+    //     c - Answer option
+    
+    // Question 3 - Who was Marie Curie?
+        // a - Answer option
+        // b - Answer option
+        // c - Answer option
+    
+    // Question 4 - Who was Nikola Tesla?
+        // a - Answer option
+        // b - Answer option
+        // c - Answer option
+
+
+    // The answer is either recorded as correct or incorrect and data updated
+
+    // When incorrect time is erased from the clock
+
+// The score is kept and then tallied at the end
