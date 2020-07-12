17f2732bd8a3a24eeb388946aa10ad0c56753fbd
T Lom <tiffanyplombardi@gmail.com>
2020-07-12 17:40:16 -0400

    setting up butttons and their variables

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Dependencies DOM Elements ==============================
-// Start Data
+var startButton = document.getElementById("StartBtn");
+var nextButton = document.getElementById("NextBtn");
 
 // create variable
 var score = 0;
@@ -52,56 +53,6 @@ var quiz = [
       "Experimented with wireless power transmission by electrifying butterflies and often left residents without power in Colorado",
   },
 ];
-
-// Var responses - button that is a larger box and clickable with the answer in the box the answer is in the button
-
-// get the questions into the buttons
-// give answer and populate next question and add 25 points to the score
-// Create a function that populates the buttons
-// create a scoring function
-// create function that "secondsLeft" where 5 seconds is subtracted on wrong answers
-
-// User Interaction =======================================
-// First Window with a Greeting called Coding Quiz Challenge that is timed
-
-// Call Funcitons =========================================
-// create a function that is basedon all the things thta need to happen when click start
-// the start button disappears
-// clock starts
-// frst question and set of answers shows up
-onclick = function () {
-  var startEl = document.getElementById("StartBtn");
-  console.log();
-  StartBtn.addEventListener;
-  // access the start button screen element
-  startEl.style.display = "none";
-  console.log();
-
-  function runClock() {
-    time--;
-    timerEl.textContent = time;
-    console.log();
-    // if time <= 0 then end quiz
-  }
-
-  //   The buttons for the questions have to pop up and be within containers/boxes
-  // Click button and a question pops up
-  var currentQuestionI = 0;
-  var questionTitle = document.getElementById("currQuestion");
-  function getQuestion() {
-    var currQuestion = quiz[Question[i]];
-    questionTitle.textContent = currQuestion.Question;
-    getQuestion();
-    console.log();
-  }
-};
-
-function getClick() {}
-
-function checkAnswers() {
-  // the function would take the user's selected answer and match it up against each option in answerKey array with .include method which means that if its included its true
-  // result - if there is a match then +25 points if no match subtract 5 seconds
-}
 
 // Start Button - add event listener for whne the button is clicked
 
